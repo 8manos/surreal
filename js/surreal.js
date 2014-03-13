8cdd92072dc802bd780351e6bfa08d22ed091a9d
Anabelle Handdoek <github@huellaspyp.com>
2014-03-13 14:59:50 -0500

    deshabilita zoom móvil, lazyload imgs owl, hashchange para links a paginas e historial

--- js/surreal.js
+++ js/surreal.js
@@ -1,6 +1,7 @@
 $(document).on('ready', function(){
 	console.log("Starting...");
 
+
 	$('#cabeza .toggle').on( 'click', function(e){
 		e.preventDefault();
 		if( $('body').hasClass('show-menu') ){
@@ -11,11 +12,26 @@ $(document).on('ready', function(){
 	});
 
 	$(".owl-carousel").owlCarousel({
+		lazyLoad: true,
+		lazyFollow: true,
 		singleItem: true,
 		navigation: true,
 		paginationNumbers: true,
-		addClassActive: true
+		addClassActive: true,
+		afterMove: function( slide ){ 
+			location.hash = "page-" + $('.owl-item.active').index();
+		}
 	});
 
 	var owl = $(".owl-carousel").data('owlCarousel');
+
+	if( location.hash ){
+		var pagina = location.hash.replace('#page-','');
+		owl.jumpTo( pagina)
+	}
+	
+	$(window).on( "hashchange", function( event ) {
+		var pagina = location.hash.replace('#page-','');
+		owl.goTo( pagina)
+	});
 });

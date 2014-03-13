0a32b42088073b8056e07669e532ad28630e472c
Anabelle Handdoek <github@huellaspyp.com>
2014-03-13 16:25:34 -0500

    Mapea teclas de cursor, media query portrait grande y arregla bug con parseInt

--- js/surreal.js
+++ js/surreal.js
@@ -2,6 +2,7 @@ $(document).on('ready', function(){
 	console.log("Starting...");
 
 
+	// Menu con toggle para quienes lo soportan
 	$('#cabeza .toggle').on( 'click', function(e){
 		e.preventDefault();
 		if( $('body').hasClass('show-menu') ){
@@ -11,57 +12,77 @@ $(document).on('ready', function(){
 		}
 	});
 
+	// Inicializamos carousel
 	$(".owl-carousel").owlCarousel({
 		lazyLoad: true,
 		lazyFollow: true,
 		singleItem: true,
-		navigation: true,
 		paginationNumbers: true,
 		addClassActive: true,
 		afterInit: function() { setTimeout( paginaCompleta , 250); },
 		afterMove: function( slide ){ 
 			// Modificamos el hash para cada pagina
-			location.hash = "page-" + $('.owl-item.active').index();
+			setTimeout( function(){
+				location.hash = "page-" + $('.owl-item.active').index();
+			}, 10 );
+		}
+	});
+	var owl = $(".owl-carousel").data('owlCarousel');
+
+	// Mapeamos controles de teclado
+	$(document).keydown(function(e){
+		if (e.keyCode == 37) { 
+		   owl.prev();
+		   return false;
+		}
+		if (e.keyCode == 39) { 
+		   owl.next();
+		   return false;
 		}
 	});
 
-	var owl = $(".owl-carousel").data('owlCarousel');
-
-	// Vamos a pagina solicitada al comienzo
-	if( location.hash ){
-		var pagina = location.hash.replace('#page-','');
-		owl.jumpTo( pagina)
-	}
 
 	// Escuchamos cambios en el hash
+	var hashTimeout; 
 	$(window).on( "hashchange", function( event ) {
-		var pagina = location.hash.replace('#page-','');
-		owl.goTo( pagina)
+		if (hashTimeout) {
+			clearTimeout(hashTimeout);
+			hashTimeout = null;
+		}
+
+		hashTimeout = setTimeout( function(){
+			var pagina = location.hash.replace('#page-','');
+			owl.goTo( parseInt(pagina) );
+		}, 10 );
 	});
 
 
 	// Nos aseguramos que la pagina se vea completa en carousel
 	function paginaCompleta(){
 		var	altoCarousel = $('.owl-wrapper-outer').height(),
-		    altoDisponible = $(window).height() - $('#cabeza').height();
+			altoDisponible = $(window).height() - $('#cabeza').height() - 50;
 
-		console.log( "Carousel: ", altoCarousel );
-		console.log( "Disponible: ", altoDisponible );
-
-		if( altoCarousel > altoDisponible ){
-			$('.owl-wrapper-outer').height( altoDisponible - 50 );
+		if( altoCarousel >= altoDisponible ){
+			$('.owl-wrapper-outer').height( altoDisponible );
+		}else{
+			$('.owl-wrapper-outer').css( 'height', 'auto');
 		}
 
 	}
 
+	// Cosas para hacer cuando ya todo haya cargado
 	$(window).load(function(){
 
-		// preferred v2, timeout instead of interval - no unnecessary ticks
 		var scrollTimeout; 
 
+		// Vamos a pagina solicitada al comienzo
+		if( location.hash ){
+			var pagina = location.hash.replace('#page-','');
+			owl.jumpTo( pagina)
+		}
+		
 		$(window).on( 'resize', function () {
 			if (scrollTimeout) {
-				// clear the timeout, if one is pending
 				clearTimeout(scrollTimeout);
 				scrollTimeout = null;
 			}

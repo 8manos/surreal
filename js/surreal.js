c0d90f78fe5c3f256d20e8217a3a561d348c9602
Anabelle Handdoek <github@huellaspyp.com>
2014-03-13 15:37:54 -0500

    pagina completa vertical siempre

--- js/surreal.js
+++ js/surreal.js
@@ -18,20 +18,55 @@ $(document).on('ready', function(){
 		navigation: true,
 		paginationNumbers: true,
 		addClassActive: true,
+		afterInit: function() { setTimeout( paginaCompleta , 250); },
 		afterMove: function( slide ){ 
+			// Modificamos el hash para cada pagina
 			location.hash = "page-" + $('.owl-item.active').index();
 		}
 	});
 
 	var owl = $(".owl-carousel").data('owlCarousel');
 
+	// Vamos a pagina solicitada al comienzo
 	if( location.hash ){
 		var pagina = location.hash.replace('#page-','');
 		owl.jumpTo( pagina)
 	}
-	
+
+	// Escuchamos cambios en el hash
 	$(window).on( "hashchange", function( event ) {
 		var pagina = location.hash.replace('#page-','');
 		owl.goTo( pagina)
+	});
+
+
+	// Nos aseguramos que la pagina se vea completa en carousel
+	function paginaCompleta(){
+		var	altoCarousel = $('.owl-wrapper-outer').height(),
+		    altoDisponible = $(window).height() - $('#cabeza').height();
+
+		console.log( "Carousel: ", altoCarousel );
+		console.log( "Disponible: ", altoDisponible );
+
+		if( altoCarousel > altoDisponible ){
+			$('.owl-wrapper-outer').height( altoDisponible - 50 );
+		}
+
+	}
+
+	$(window).load(function(){
+
+		// preferred v2, timeout instead of interval - no unnecessary ticks
+		var scrollTimeout; 
+
+		$(window).on( 'resize', function () {
+			if (scrollTimeout) {
+				// clear the timeout, if one is pending
+				clearTimeout(scrollTimeout);
+				scrollTimeout = null;
+			}
+			scrollTimeout = setTimeout( paginaCompleta , 150);
+		});
+
 	});
 });

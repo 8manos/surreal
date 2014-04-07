5cbf64103ca78313772b330efc5c66f78c797ebc
Anabelle Handdoek <github@huellaspyp.com>
2014-04-07 17:00:06 -0500

    prueba de imgs segun tamaño de pantalla

--- js/surreal.js
+++ js/surreal.js
@@ -1,4 +1,21 @@
 $(document).on('ready', function(){
+	// Medidas
+	var pantalla_alto, pantalla_ancho;
+
+	function medidas(){
+		pantalla_alto = $(window).height() - $('#cabeza').height(),
+		pantalla_ancho = $(window).width();
+
+		console.log( pantalla_ancho, pantalla_alto );
+	}
+	medidas();
+
+	// Configuramos fuentes segun pantalla
+	$('img.lazyOwl').each(function (i) {
+		var img_medida = $(this).data('src').replace("/xx/", "/"+pantalla_ancho+"/").replace("/yy/", "/"+pantalla_alto+"/");
+		$(this).data('src', img_medida );
+	});
+
 
 	// Menu con toggle para quienes lo soportan
 	$('#cabeza .toggle').on( 'click', function(e){
@@ -7,34 +24,6 @@ $(document).on('ready', function(){
 			$('body').removeClass('show-menu');
 		}else{
 			$('body').addClass('show-menu');
-		}
-	});
-
-	// Inicializamos carousel
-	$(".owl-carousel").owlCarousel({
-		lazyLoad: true,
-		singleItem: true,
-		paginationNumbers: true,
-		addClassActive: true,
-		afterInit: function() { setTimeout( paginaCompleta , 250); },
-		afterMove: function( slide ){ 
-			// Modificamos el hash para cada pagina
-			setTimeout( function(){
-				location.hash = "page-" + $('.owl-item.active').index();
-			}, 10 );
-		}
-	});
-	var owl = $(".owl-carousel").data('owlCarousel');
-
-	// Mapeamos controles de teclado
-	$(document).keydown(function(e){
-		if (e.keyCode == 37) { 
-		   owl.prev();
-		   return false;
-		}
-		if (e.keyCode == 39) { 
-		   owl.next();
-		   return false;
 		}
 	});
 
@@ -65,20 +54,6 @@ $(document).on('ready', function(){
 		});
 	});
 
-	// Escuchamos cambios en el hash
-	var hashTimeout; 
-	$(window).on( "hashchange", function( event ) {
-		if (hashTimeout) {
-			clearTimeout(hashTimeout);
-			hashTimeout = null;
-		}
-
-		hashTimeout = setTimeout( function(){
-			var pagina = location.hash.replace('#page-','');
-			owl.goTo( parseInt(pagina) );
-		}, 10 );
-	});
-
 
 	// Nos aseguramos que la pagina se vea completa en carousel
 	function paginaCompleta(){
@@ -96,6 +71,34 @@ $(document).on('ready', function(){
 	// Cosas para hacer cuando ya todo haya cargado
 	$(window).load(function(){
 
+		// Inicializamos carousel
+		$(".owl-carousel").owlCarousel({
+			lazyLoad: true,
+			singleItem: true,
+			paginationNumbers: true,
+			addClassActive: true,
+			afterInit: function() { setTimeout( paginaCompleta , 250); },
+			afterMove: function( slide ){ 
+				// Modificamos el hash para cada pagina
+				setTimeout( function(){
+					location.hash = "page-" + $('.owl-item.active').index();
+				}, 10 );
+			}
+		});
+		var owl = $(".owl-carousel").data('owlCarousel');
+
+		// Mapeamos controles de teclado
+		$(document).keydown(function(e){
+			if (e.keyCode == 37) { 
+			   owl.prev();
+			   return false;
+			}
+			if (e.keyCode == 39) { 
+			   owl.next();
+			   return false;
+			}
+		});
+
 		var scrollTimeout; 
 
 		// Vamos a pagina solicitada al comienzo
@@ -112,5 +115,18 @@ $(document).on('ready', function(){
 			scrollTimeout = setTimeout( paginaCompleta , 150);
 		});
 
+		// Escuchamos cambios en el hash
+		var hashTimeout; 
+		$(window).on( "hashchange", function( event ) {
+			if (hashTimeout) {
+				clearTimeout(hashTimeout);
+				hashTimeout = null;
+			}
+
+			hashTimeout = setTimeout( function(){
+				var pagina = location.hash.replace('#page-','');
+				owl.goTo( parseInt(pagina) );
+			}, 10 );
+		});
 	});
 });
